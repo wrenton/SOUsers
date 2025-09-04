04ebad84c258abb7f9af9eac98bc72475686f0fd
Will Renton <>
2025-09-04 11:12:56 +0100

    Initial project setup.

---
+++ SOUsers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SOUsers
+//
+//  Created by Will Renton on 04/09/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SOUsersTests/SOUsersTests.swift
@@ -0,0 +1,17 @@
+//
+//  SOUsersTests.swift
+//  SOUsersTests
+//
+//  Created by Will Renton on 04/09/2025.
+//
+
+import Testing
+@testable import SOUsers
+
+struct SOUsersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
